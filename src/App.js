a30a3cf9414f0bd18ec48fedf5e9b78c2f7f3ad0
Christos Gkoumas <cgoumas@skroutz.gr>
2020-11-07 13:15:33 +0200

    Update app with 13033 stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 import './App.css';
 
 function App() {
+  const defaultValue = '';
+
+  const [firstName, setFirstName] = useState(() => {
+    const stickyValue = window.localStorage.getItem('storageFirstName');
+    return stickyValue !== null
+      ? JSON.parse(stickyValue)
+      : defaultValue;
+  });
+
+  const [lastName, setLastName] = useState(() => {
+    const stickyValue = window.localStorage.getItem('storageLastName');
+    return stickyValue !== null
+      ? JSON.parse(stickyValue)
+      : defaultValue;
+  });
+
+  const [address, setAddress] = useState(() => {
+    const stickyValue = window.localStorage.getItem('storageAddress');
+    return stickyValue !== null
+      ? JSON.parse(stickyValue)
+      : defaultValue;
+  });
+
+  const [reason, setReason] = useState(() => {
+    const stickyValue = window.localStorage.getItem('storageReason');
+    return stickyValue !== null
+      ? JSON.parse(stickyValue)
+      : defaultValue;
+  });
+
+  const reasons = [
+    {
+      id: '',
+      reason: 'Λόγος Μετακίνησης',
+      disabled: true
+    },
+    {
+      id: '1',
+      reason: 'Μετακίνηση για λόγους υγείας',
+      disabled: false
+    },
+    {
+      id: '2',
+      reason: 'Μετάβαση σε εν λειτουργία κατάστημα ειδών πρώτης ανάγκης',
+      disabled: false
+    },
+    {
+      id: '3',
+      reason: 'Μετάβαση σε δημόσια υπηρεσία',
+      disabled: false
+    },
+    {
+      id: '4',
+      reason: 'Μετάβαση για παροχή βοήθειας σε ανθρώπους που βρίσκονται σε ανάγκη',
+      disabled: false
+    },
+    {
+      id: '5',
+      reason: 'Μετάβαση σε κηδεία',
+      disabled: false
+    },
+    {
+      id: '6',
+      reason: 'Σωματική άσκηση σε εξωτερικό χώρο ή κίνηση με κατοικίδιο ζώο',
+      disabled: false
+    }
+  ];
+
+  const compileSMSMessage = () => {
+    if (firstName && lastName && address && reason) {
+      let sms = `sms://13033?body=${reason} ${firstName.toUpperCase()} `;
+      sms += `${lastName.toUpperCase()} ${address.toUpperCase()}`;
+      return (
+        <a href={sms} type="button" className="btn btn-primary">Αποστολή SMS στο 13033</a>
+      )
+    }
+
+    return null;
+  }
+
+  useEffect(() => {
+    window.localStorage.setItem('storageFirstName', JSON.stringify(firstName));
+    window.localStorage.setItem('storageLastName', JSON.stringify(lastName));
+    window.localStorage.setItem('storageAddress', JSON.stringify(address));
+    window.localStorage.setItem('storageReason', JSON.stringify(reason));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-1 m-1">
+      <h4>Βγες έξω!</h4>
+      <form className="m-1">
+        <div className="form-row m-4">
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Όνομα"
+              value={firstName} onChange={event => setFirstName(event.target.value)}/>
+          </div>
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Επίθετο"
+              value={lastName}
+              onChange={event => setLastName(event.target.value)}/>
+          </div>
+        </div>
+        <div className="form-row m-4">
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Διεύθυνση"
+              value={address}
+              onChange={event => setAddress(event.target.value)}/>
+          </div>
+        </div>
+        <div className="form-row m-4">
+          <div className="col">
+            <select className="form-control" value={reason} onChange={event => setReason(event.target.value)}>
+              {reasons.map((value, i) => {
+                return (
+                  <option key={i} value={value.id} disabled={value.disabled}>{value.reason}</option>
+                );
+              })}
+            </select>
+          </div>
+        </div>
+      </form>
+      {compileSMSMessage()}
     </div>
   );
 }
